d3ac933f0bc6e1f78a46c4b230b8504569613439
ten986 <tenkichi986@yahoo.co.jp>
2019-05-21 03:48:14 +0900

    img2

--- rotatebot.py
+++ rotatebot.py
@@ -12,5 +12,5 @@ twitter = OAuth1Session(os.environ["CONSUMER_KEY"],  os.environ["CONSUMER_SECRET
 img_file = 'tokei.jpeg'
 b64 = base64.encodestring(open(img_file, 'rb').read())
 
-params = {"image": b64p}
+params = {"image": b64}
 req = twitter.post("https://api.twitter.com/1.1/account/update_profile_image.json", params = params)
