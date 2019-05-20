ffa2a1ab6f7dcfcbdcefc1305b9e9837208680a6
ten986 <tenkichi986@yahoo.co.jp>
2019-05-21 04:23:12 +0900

    b64

--- rotatebot.py
+++ rotatebot.py
@@ -9,11 +9,12 @@ import base64
 
 twitter = OAuth1Session(os.environ["CONSUMER_KEY"],  os.environ["CONSUMER_SECRET"], os.environ["ACCESS_TOKEN_KEY"], os.environ["ACCESS_TOKEN_SECRET"])
 
-img_file = 'tokei.jpeg'
+img_file = 'tokei3.jpeg'
 b64 = base64.encodestring(open(img_file, 'rb').read())
+
+print(b64)
 
 params = {"image": b64}
 res = twitter.post("https://api.twitter.com/1.1/account/update_profile_image.json", params = params)
 
 print(res.status_code)
-print(json.loads(res._content)['error'])
