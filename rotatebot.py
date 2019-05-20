6e46cd7c1ad940247c517a894ee6b6d53a8fc5b2
ten986 <tenkichi986@yahoo.co.jp>
2019-05-21 03:46:40 +0900

    img

--- rotatebot.py
+++ rotatebot.py
@@ -5,15 +5,12 @@ import json
 import os
 import random
 import datetime
+import base64
 
 twitter = OAuth1Session(os.environ["CONSUMER_KEY"],  os.environ["CONSUMER_SECRET"], os.environ["ACCESS_TOKEN_KEY"], os.environ["ACCESS_TOKEN_SECRET"])
 
-tweets = ["文言1","文言2","文言3"]
+img_file = 'tokei.jpeg'
+b64 = base64.encodestring(open(img_file, 'rb').read())
 
-randomtweet = tweets[random.randrange(len(tweets))]
-
-timestamp = datetime.datetime.today() + datetime.timedelta(hours=9)
-timestamp = str(timestamp.strftime("%Y/%m/%d %H:%M"))
-
-params = {"status": randomtweet + " " + timestamp}
-req = twitter.post("https://api.twitter.com/1.1/statuses/update.json", params = params)
+params = {"image": b64p}
+req = twitter.post("https://api.twitter.com/1.1/account/update_profile_image.json", params = params)
